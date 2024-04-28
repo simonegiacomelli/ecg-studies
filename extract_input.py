43dee1ee299d326c3972e45f694aa8bd5bd04de9
Simone Giacomelli <simone.giacomelli@gmail.com>
2024-04-28 21:20:15 +0200

    extract input to csv

--- extract_input.py
+++ extract_input.py
@@ -5,8 +5,13 @@ from wfdb import rdrecord, plot_wfdb
 directory = Path('dataset/fantasia')
 records = sorted([f for f in directory.iterdir() if f.is_file() and f.suffix == '.hea'])
 
-for record_path in records:
-    record = rdrecord(str(record_path.with_suffix('')))
+directory_csv = Path('dataset/fantasia_csv')
+directory_csv.mkdir(exist_ok=True)
 
-    plot_wfdb(record, title="Record - %s" % record.record_name)
-    input("Press enter to continue...")
+for record_path in records:
+    record = rdrecord(str(record_path.with_suffix('')),channel_names=['ECG'])
+    record1d = record.p_signal.flatten()
+    print(record_path.stem + ' len: ' + str(len(record1d)))
+    csv_path = directory_csv / (record_path.stem + '.csv')
+    # save numpy array to csv file
+    record1d.tofile(csv_path, sep=',')
